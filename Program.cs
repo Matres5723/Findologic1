2feb8afc587b97c34b6aba3789d5ab831f37aeb0
Matres5723 <69048954+Matres5723@users.noreply.github.com>
2020-07-31 18:01:18 +0200

    Dateien

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Findologic4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bitte geben Sie den Shopkey ein: ");
+            string Shopkeysuche = Console.ReadLine();
+
+            Console.WriteLine(string.Join("", readRecord(Shopkeysuche, "logging.csv", 1)));
+        }
+        public static string[] readRecord(string searchTerm, string filepath, int positionOfSearchTerm)
+        {
+            positionOfSearchTerm--;
+            string[] recordNotFound = { "Record not found" };
+
+
+            try
+            {
+                string[] lines = System.IO.File.ReadAllLines(filepath);
+
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    string[] fields = lines[i].Split("\t");
+                    if (recordMatches(searchTerm, fields, positionOfSearchTerm))
+                    {
+                        Console.WriteLine("Record found");
+                        return fields;
+                    }
+                }
+
+                return recordNotFound;
+            }
+            catch(Exception ex)
+            {
+                return recordNotFound;
+            }
+
+        }
+
+        public static bool recordMatches(string searchTerm, string[] record, int positionOfSearchTerm)
+        {
+            if (record[positionOfSearchTerm].Equals(searchTerm))
+            {
+                return true;
+            }
+            return false;
+        }    
+    }
+}
